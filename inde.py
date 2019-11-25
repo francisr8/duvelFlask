cd2dc01b7e209182d7a86c635af81d832455ae19
Francis Rousseau <Francis.rousseau@student.kdg.be>
2019-11-25 17:14:53 +0100

    test

--- inde.py
+++ inde.py
@@ -1,15 +1,16 @@
 from flask import Flask, request, jsonify
-#from webbot import Browser
+from webbot import Browser
 app = Flask(__name__)
-#web = Browser()
 
 
 @app.route('/refresh/', methods=['GET'])
 def respond():
+
+
     # Retrieve the name from url parameter
     #name = request.args.get("refresh", None)
     response = {}
-    response["MESSAGE"] = "IDK"
+    response["MESSAGE"] = read()
 
     return jsonify(response)
 
@@ -18,7 +19,20 @@ def index():
     return "Welcome to my json server, if you want to get the data, please use refresh url"
 
 
+def initialize():
+    try:
+
+        return "HEY FIJNE DAG"
+    except:
+        print("Error")
+        return "Er is een fout gebeurd"
+
+def read():
+    web = Browser()
+    web.go_to('http://sales.duvel.com/Account/Login.aspx')
+    return web.get_title()
+
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
+    app.run(threaded=True, port=4000)
