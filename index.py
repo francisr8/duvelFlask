c944bc5c8af641a27c153d34af99a642b1868614
Francis Rousseau <francis.rousseau@student.kdg.be>
2019-12-12 12:56:10 +0100

    fix index error

--- index.py
+++ index.py
@@ -56,15 +56,48 @@ def initialize():
     select.select_by_value('2')
     web.find_element_by_id('ctl00_MainContent_ReportViewer1_ctl04_ctl00').click()
     web.implicitly_wait(1)
-    element = web.find_element(By.XPATH,"/html/body/div/span/div/table/tbody/tr[5]/td[3]/div/div[1]/div/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr[4]")
-    array = element.text.replace("  ", "\n").split("\n")
-    info = Info(array[0], array[1], array[2], array[3], array[4], array[5], array[6], array[7], array[8], array[9],
-                array[10], array[11], array[12], array[13], array[14])
     array2 = []
-    array2.append(info.__dict__)
-    array2.append(info.__dict__)
-    #s = json.dumps(array2)
-    return array2
+    dates = []
+    json = {}
+    lengte = 0
+    rows = web.find_elements(By.XPATH,
+                             "/html/body/div/span/div/table/tbody/tr[5]/td[3]/div/div[1]/div/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr")
+    print len(rows)
+    for i in range(2, len(rows) + 1):
+        subjson = {}
+        element = web.find_element(By.XPATH,
+                                   "/html/body/div/span/div/table/tbody/tr[5]/td[3]/div/div[1]/div/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr[" + str(
+                                       i) + "]")
+
+        array = element.text.replace("  ", "\n").split("\n")
+        if "Monday" in array[0] or "Tuesday" in array[0] or "Wednesday" in array[0] or "Thursday" in array[
+            0] or "Friday" in array[0] or "Saturday" in array[0]:
+            dates.append(array[0])
+        elif not "Uur" in array[0] and array[0] != "":
+            subjson["date"] = dates[len(dates) - 1]
+            subjson["uur"] = array[0]
+            subjson["resNr"] = array[1]
+            subjson["Product"] = array[2]
+            subjson["Klantnaam"] = array[3]
+            subjson["Groep"] = array[4]
+            subjson["Mobiel"] = array[5]
+            subjson["Aantal"] = array[6]
+            subjson["Gids1"] = array[7]
+            subjson["Gids2"] = array[8]
+            subjson["Tapper"] = array[9]
+            subjson["Shop"] = array[10]
+            subjson["Opmerkingen"] = array[11]
+            subjson["Status"] = array[12]
+            subjson["Prijs"] = array[13]
+            subjson["Betaald"] = array[14]
+            lengte += 1
+            json[lengte] = subjson
+
+        # info = Info(array[0], array[1], array[2], array[3], array[4], array[5], array[6], array[7], array[8], array[9],
+        # array[10], array[11], array[12], array[13], array[14])
+        # array2.append(info.__dict__)
+
+    return json
 
 
 
